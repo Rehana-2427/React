4193d75a58f8831c2251889f0423e02c4fd11df9
Rehana2427 <shaikrehana248@gmail.com>
2025-05-02 18:56:20 +0530

    commit changes started 3 part Lifecyclemethods of a component

--- Ecosystem_2/React_Setup/src/index.js
+++ Ecosystem_2/React_Setup/src/index.js
@@ -1,6 +1,6 @@
-import React  from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style.css"
+import "./style.css";
 
 const heading = React.createElement("h1",{id:'title',className:"container"},"Intrdouction to react")
 
